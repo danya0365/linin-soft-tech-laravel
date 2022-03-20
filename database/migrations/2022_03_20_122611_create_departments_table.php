9fc00184131d3a6c9be25b57dcdbaa5c78b96273
Marosdee Uma <danya0365@gmail.com>
2022-03-20 20:23:02 +0700

    chore: add departments fields

--- database/migrations/2022_03_20_122611_create_departments_table.php
+++ database/migrations/2022_03_20_122611_create_departments_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('departments', function (Blueprint $table) {
             $table->id();
+            $table->string('name'); // (ซัก, อบ, รีด, พับแพ็ค, จัดเก็บ)
             $table->timestamps();
         });
     }
