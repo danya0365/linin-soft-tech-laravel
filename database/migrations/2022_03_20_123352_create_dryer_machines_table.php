717043b0b73544be6621310d039e5c636f8be02f
Marosdee Uma <danya0365@gmail.com>
2022-03-20 20:34:57 +0700

    chore: add dryer_machines fields

--- database/migrations/2022_03_20_123352_create_dryer_machines_table.php
+++ database/migrations/2022_03_20_123352_create_dryer_machines_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('dryer_machines', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('photo');
+            $table->integer('maximum_weight');
             $table->timestamps();
         });
     }
