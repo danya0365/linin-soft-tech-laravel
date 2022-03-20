fecf65aa3b74703b72aa99641a20b8a3f22676cc
Marosdee Uma <danya0365@gmail.com>
2022-03-20 20:25:23 +0700

    chore: add employees fields

--- database/migrations/2022_03_20_122710_create_employees_table.php
+++ database/migrations/2022_03_20_122710_create_employees_table.php
@@ -15,6 +15,11 @@ return new class extends Migration
     {
         Schema::create('employees', function (Blueprint $table) {
             $table->id();
+            $table->string('code');
+            $table->string('password');
+            $table->string('name');
+            $table->string('photo');
+            $table->integer('department_id');
             $table->timestamps();
         });
     }
