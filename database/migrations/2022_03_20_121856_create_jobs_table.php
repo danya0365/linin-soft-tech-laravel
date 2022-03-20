066d573e39300dac2d74bf213e2dd524e86e6a18
Marosdee Uma <danya0365@gmail.com>
2022-03-20 19:49:00 +0700

    chore: add jobs fields

--- database/migrations/2022_03_20_121856_create_jobs_table.php
+++ database/migrations/2022_03_20_121856_create_jobs_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('jobs', function (Blueprint $table) {
             $table->id();
+            $table->integer('job_group_id');
+            $table->integer('customer_id');
+            $table->integer('employee_id');
+            $table->enum('job_type', ['new', 'edit']);
+            $table->integer('washing_machine_id')->nullable();
+            $table->integer('dryer_machine_id')->nullable();
+            $table->integer('laundry_type_id')->nullable();
+            $table->integer('wet_weight')->nullable();
+            $table->string('color')->nullable();
             $table->timestamps();
         });
     }
