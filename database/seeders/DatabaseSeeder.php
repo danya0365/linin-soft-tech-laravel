c6ea4e4006130b659a38dc9fad47ce8e85a98c62
Marosdee Uma <danya0365@gmail.com>
2022-03-20 22:28:01 +0700

    chore: add InventorySeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             DepartmentSeeder::class,
             LinenSeeder::class,
-            EnergyResourceSeeder::class
+            EnergyResourceSeeder::class,
+            InventorySeeder::class
         ]);
     }
 }
