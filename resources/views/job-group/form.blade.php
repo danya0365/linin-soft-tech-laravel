1ac4d6a500c23c10ee21c354eb870c35cbf5186e
Marosdee Uma <danya0365@gmail.com>
2022-03-28 23:26:10 +0700

    style: update css class name

--- resources/views/job-group/form.blade.php
+++ resources/views/job-group/form.blade.php
@@ -33,7 +33,7 @@
         </div>
 
     </div>
-    <div class="box-footer mt20">
+    <div class="box-footer mt-4">
         <button type="submit" class="btn btn-primary">Submit</button>
     </div>
 </div>
