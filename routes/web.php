99279bfb7cc3c69a3671698f36d3143cc0e164bc
Marosdee Uma <danya0365@gmail.com>
2022-04-06 21:39:12 +0700

    chore: collect select linen product

--- routes/web.php
+++ routes/web.php
@@ -232,6 +232,8 @@ Route::group(['prefix' => 'worker', 'middleware' => ['auth']], function () {
             Route::get('/{operationId}/operation-linen-product', [App\Http\Controllers\Worker\Operation\CollectController::class, 'selectOperationLinenProduct'])->name('worker.operation.collect.select-operation-linen-product');
             Route::get('/{operationId}/linen-product/{operationLinenProductId}/linen-case', [App\Http\Controllers\Worker\Operation\CollectController::class, 'selectLinenCase'])->name('worker.operation.collect.select-linen-case');
             Route::get('/{operationId}/linen-product/{operationLinenProductId}/linen-case/{linenCase}', [App\Http\Controllers\Worker\Operation\CollectController::class, 'setSelectLinenCase'])->name('worker.operation.collect.set-select-linen-case');
+            Route::get('/{operationId}/linen-product/{operationLinenProductId}/linen-product', [App\Http\Controllers\Worker\Operation\CollectController::class, 'selectLinenProduct'])->name('worker.operation.collect.select-linen-product');
+            Route::get('/{operationId}/linen-product/{operationLinenProductId}/linen-product/{linenProductId}', [App\Http\Controllers\Worker\Operation\CollectController::class, 'setSelectLinenProduct'])->name('worker.operation.collect.set-select-linen-product');
         });
     });
 
