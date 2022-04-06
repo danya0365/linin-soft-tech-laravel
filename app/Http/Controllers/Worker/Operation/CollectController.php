21a57d0c610268df9d5d388d4e87f599f2371a08
Marosdee Uma <danya0365@gmail.com>
2022-04-06 21:35:29 +0700

    chore: collect select linen case

--- app/Http/Controllers/Worker/Operation/CollectController.php
+++ app/Http/Controllers/Worker/Operation/CollectController.php
@@ -113,4 +113,16 @@ class CollectController extends Controller
         $operationLinenCases = OperationLinenCase::$list;
         return view('worker.operations.collect.select-linen-case', ['operation' => $operation->toArray(), 'operationLinenProduct' => $operationLinenProduct, 'operationLinenCases' => $operationLinenCases]);
     }
+
+    public function setSelectLinenCase($operationId, $operationLinenProductId, $linenCase)
+    {
+        $operationLinenProduct = OperationLinenProduct::find($operationLinenProductId);
+        $operationLinenProduct->linen_case = $linenCase;
+        $operationLinenProduct->save();
+
+        $operation = Operation::find($operationId);
+        $operation->updateRelateFields();
+
+        return redirect(route('worker.operation.collect.select-linen-product', ['operationId' => $operation->id, 'operationLinenProductId' => $operationLinenProduct->id]));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -231,6 +231,7 @@ Route::group(['prefix' => 'worker', 'middleware' => ['auth']], function () {
             Route::get('/{operationId}/in-progress', [App\Http\Controllers\Worker\Operation\CollectController::class, 'setInProgress'])->name('worker.operation.collect.set-in-progress');
             Route::get('/{operationId}/operation-linen-product', [App\Http\Controllers\Worker\Operation\CollectController::class, 'selectOperationLinenProduct'])->name('worker.operation.collect.select-operation-linen-product');
             Route::get('/{operationId}/linen-product/{operationLinenProductId}/linen-case', [App\Http\Controllers\Worker\Operation\CollectController::class, 'selectLinenCase'])->name('worker.operation.collect.select-linen-case');
+            Route::get('/{operationId}/linen-product/{operationLinenProductId}/linen-case/{linenCase}', [App\Http\Controllers\Worker\Operation\CollectController::class, 'setSelectLinenCase'])->name('worker.operation.collect.set-select-linen-case');
         });
     });
 
