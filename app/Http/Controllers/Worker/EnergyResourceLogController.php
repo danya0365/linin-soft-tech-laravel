90d9c232b02da10816629d9c3fbc45de5c42fab3
Marosdee Uma <danya0365@gmail.com>
2022-04-16 13:01:07 +0700

    chore: submit log electricity

--- app/Http/Controllers/Worker/EnergyResourceLogController.php
+++ app/Http/Controllers/Worker/EnergyResourceLogController.php
@@ -89,4 +89,32 @@ class EnergyResourceLogController extends Controller
             ]
         );
     }
+
+    public function submitElectricityLog($energyResourceLogId)
+    {
+        $energyResourceLog = EnergyResourceLog::with('employee')->find($energyResourceLogId);
+        $dateStartAt = request()->get('date_start_at');
+        $dateEndAt = request()->get('date_end_at');
+
+        if (request()->isMethod('post')) {
+
+            request()->validate(['value' => 'required', 'log_date' => 'required']);
+
+            $energyResourceLog->log_date = request()->get('log_date');
+            $energyResourceLog->value = request()->get('value');
+            $energyResourceLog->unit = "kw/hour";
+            $energyResourceLog->save();
+
+            return redirect(route('worker.energy-resource.logs'));
+        }
+
+        return view(
+            'worker.energy-resources.submit-electricity',
+            [
+                'energyResourceLog' => $energyResourceLog,
+                'dateStartAt' => $dateStartAt,
+                'dateEndAt' => $dateEndAt
+            ]
+        );
+    }
 }
