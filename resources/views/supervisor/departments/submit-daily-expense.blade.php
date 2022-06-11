01983c24fc6b34325eb153c29fa50533df9135f3
Marosdee Uma <danya0365@gmail.com>
2022-06-11 13:33:07 +0700

    chore: attach photo and message

--- resources/views/supervisor/departments/submit-daily-expense.blade.php
+++ resources/views/supervisor/departments/submit-daily-expense.blade.php
@@ -20,33 +20,39 @@
             <div class="card">
                 <div class="card-header">เพิ่มบันทึกค่าใช้จ่ายรายวัน</div>
                 <div class="card-body">
-                    <form class="row row-cols-lg-auto g-3 mb-3" action="{{ request()->url() }}" method="POST" role="form" enctype="multipart/form-data">
+                    <form class="row g-3 mb-3" action="{{ request()->url() }}" method="POST" role="form" enctype="multipart/form-data">
                         
                         @csrf
                         <div class="col-12">
-                            <div class="input-group">
-                                <label class="input-group-text" for="department_id">แผนก</label>
-                                <select class="form-select" id="department_id" name="department_id">
-                                    <option value="">ไม่เลือก</option>
-                                    @foreach ( $departments as $department )
-                                    <option value="{{ $department->id }}">{{ $department->name }}</option>
-                                    @endforeach
-                                </select>
-                            </div>
+                            <label class="form-label" for="department_id">แผนก</label>
+                            <select class="form-select" id="department_id" name="department_id">
+                                <option value="">ไม่เลือก</option>
+                                @foreach ( $departments as $department )
+                                <option value="{{ $department->id }}">{{ $department->name }}</option>
+                                @endforeach
+                            </select>
                         </div>
 
                         <div class="col-12">
-                            <div class="input-group">
-                                <label class="input-group-text" for="daily_date">วันที่</label>
-                                <input type="date" class="form-control" id="daily_date" name="daily_date" />
-                            </div>
+                            <label class="form-label" for="daily_date">วันที่</label>
+                            <input type="date" class="form-control" id="daily_date" name="daily_date" />
                         </div>
 
                         <div class="col-12">
-                            <div class="input-group">
-                                <label class="input-group-text" for="cost">จำนวนเงิน</label>
-                                <input type="number" class="form-control" id="cost" name="cost" />
-                            </div>
+                            <label class="form-label" for="cost">จำนวนเงิน</label>
+                            <input type="number" class="form-control" id="cost" name="cost" />
+                        </div>
+
+                        <div class="col-12">
+                            {{ Form::label('message', 'บันทึกข้อความ - Note', ['class' => "form-label"]) }}
+                            {{ Form::textarea('message', '', ['class' => 'form-control' . ($errors->has('message') ? ' is-invalid' : ''), 'placeholder' => '']) }}
+                            {!! $errors->first('message', '<div class="invalid-feedback">:message</div>') !!}
+                        </div>
+
+                        <div class="col-12">
+                            {{ Form::label('image_upload', 'อัพโหลดรูป - Attach Photo', ['class' => "form-label"]) }}
+                            {{ Form::file('image_upload', ['class' => 'form-control' . ($errors->has('image_upload') ? ' is-invalid' : ''), 'placeholder' => '']) }}
+                            {!! $errors->first('image_upload', '<div class="invalid-feedback">:message</div>') !!}
                         </div>
 
                         <div class="col-12">
