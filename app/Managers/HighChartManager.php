b006b3cc2a3dec5635452add834f7dafcbdb3d90
Marosdee Uma <danya0365@gmail.com>
2022-06-18 22:16:43 +0700

    feat: add sales latest days summary

--- app/Managers/HighChartManager.php
+++ app/Managers/HighChartManager.php
@@ -7,6 +7,7 @@ use App\Managers\Manager;
 use App\Models\EnergyResource;
 use App\Models\EnergyResourceLog;
 use App\Models\Expense;
+use App\Models\Income;
 use Illuminate\Support\Facades\DB;
 
 class HighChartManager extends Manager
@@ -121,5 +122,96 @@ class HighChartManager extends Manager
         }
 
         return ['titles' => $titles, 'data' => $returnData];
+    }
+
+    public static function getSalesLatestDaysSummary(): array
+    {
+        $salesLatestDaysSummary = new SalesLatestDaysSummary;
+        return $salesLatestDaysSummary->getSalesLatestDaysSummary();
+    }
+}
+
+class SalesLatestDaysSummary
+{
+    public function getSalesLatestDaysSummary(): array
+    {
+        $currentDate = \Carbon\Carbon::now();
+        $totalDays = 7;
+        $agoDate = $currentDate->subDays($totalDays);
+
+        $incomeYearSummary = $this->incomeLatestDaysSummary($totalDays, $agoDate);
+        $expenseYearSummary = $this->expenseLatestDaysSummary($totalDays, $agoDate);
+        $profitYearSummary = $this->profitLatestDaysSummary($totalDays, $incomeYearSummary, $expenseYearSummary);
+
+        $result = $this->generateLatestDaysSummaryStructure($totalDays);
+        $titles = $result['titles'];
+
+        return [
+            'titles' => array_values($titles),
+            'data' => [
+                ['name' => 'Income', 'data' => array_values($incomeYearSummary)],
+                ['name' => 'Expense', 'data' => array_values($expenseYearSummary)],
+                ['name' => 'Profit', 'data' => array_values($profitYearSummary)],
+            ],
+        ];
+    }
+
+    private function incomeLatestDaysSummary($totalDays, $agoDate): array
+    {
+        $incomeRows = DB::table('incomes')
+            ->select(DB::raw("DATE_FORMAT(created_at, '%Y-%m-%d') as ymd"), DB::raw('SUM(amount) as total_cost'))
+            ->where('created_at', '>=', $agoDate)
+            ->groupBy('ymd')->get();
+        $result = $this->generateLatestDaysSummaryStructure($totalDays);
+        $data = $result['data'];
+
+        foreach ($incomeRows as $key => $incomeRow) {
+            $ymd = $incomeRow->ymd;
+            $data[$ymd] = $incomeRow->total_cost;
+        }
+        return $data;
+    }
+
+    private function expenseLatestDaysSummary($totalDays, $agoDate): array
+    {
+        $incomeRows = DB::table('expenses')
+            ->select(DB::raw("DATE_FORMAT(created_at, '%Y-%m-%d') as ymd"), DB::raw('SUM(amount) as total_cost'))
+            ->where('created_at', '>=', $agoDate)
+            ->groupBy('ymd')->get();
+        $result = $this->generateLatestDaysSummaryStructure($totalDays);
+        $data = $result['data'];
+
+        foreach ($incomeRows as $key => $incomeRow) {
+            $ymd = $incomeRow->ymd;
+            $data[$ymd] = $incomeRow->total_cost;
+        }
+
+        return $data;
+    }
+
+    private function profitLatestDaysSummary($totalDays, $incomeYearSummary, $expenseYearSummary): array
+    {
+        $result = $this->generateLatestDaysSummaryStructure($totalDays);
+        $data = $result['data'];
+
+        foreach ($data as $key => $profitRow) {
+            $data[$key] = $incomeYearSummary[$key] - $expenseYearSummary[$key];
+        }
+
+        return $data;
+    }
+
+    private function generateLatestDaysSummaryStructure($totalDays): array
+    {
+        $titles = $data = [];
+        $_date = \Carbon\Carbon::now();
+        for ($i = 0; $i < $totalDays; $i++) {
+            $date = $i > 0 ? $_date->subDays(1) : $_date;
+            $dateYmd = $date->format('Y-m-d');
+            $titles[$dateYmd] = $date->format('Y F j');
+            $data[$dateYmd] = 0;
+        }
+
+        return ['titles' => $titles, 'data' => $data];
     }
 }
