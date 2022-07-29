c8b4b96e7286b37c4d1f5b45784b05e833997aff
Marosdee Uma <danya0365@gmail.com>
2022-07-29 16:01:47 +0700

    chore: release gsm version

--- config/app.php
+++ config/app.php
@@ -16,7 +16,7 @@ return [
     */
 
     'name' => env('APP_NAME', 'Laravel'),
-    'version' => '1.0.0-release-candidate',
+    'version' => '1.0.0-gsm',
 
     /*
     |--------------------------------------------------------------------------
