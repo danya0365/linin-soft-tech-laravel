cf3089513c5c93aa3e6caee862c07217f21409f9
Marosdee Uma <danya0365@gmail.com>
2022-10-08 00:27:23 +0700

    feat: display expense in energy resource log

--- app/Console/Commands/OptimizeExpense.php
+++ app/Console/Commands/OptimizeExpense.php
@@ -2,8 +2,11 @@
 
 namespace App\Console\Commands;
 
+use App\Models\EnergyResourceLog;
+use App\Models\Expense;
 use Illuminate\Console\Command;
 use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Helper\TableSeparator;
 
 class OptimizeExpense extends Command
 {
@@ -28,20 +31,42 @@ class OptimizeExpense extends Command
      */
     public function handle()
     {
+        //SELECT COUNT(*), CONCAT(`table_name`, "-", `table_id`) as tableName FROM `expenses` GROUP BY tableName HAVING COUNT(*) > 1;
+
         // Create a new Table instance.
         $table = new Table($this->output);
 
+        // Create a new TableSeparator instance.
+        $separator = new TableSeparator;
+
         // Set the table headers.
         $table->setHeaders([
-            'Site', 'Description'
+            'Log ID', 'Resource Name', 'Cost', '', 'Expense Value'
         ]);
 
+        $rows = [];
+
+        $energyResourceLogs = EnergyResourceLog::query()->with("energyResource")->whereNotNull('cost')->take(1000)->get();
+        foreach ($energyResourceLogs as $key => $energyResourceLog) {
+
+            $tableName = app(EnergyResourceLog::class)->getTable();
+            $expense = Expense::where("table_name", $tableName)->where("table_id", $energyResourceLog->id)->first();
+            $expenseAmount = $expense->amount ?? 0;
+            $rows[] = [
+                $energyResourceLog->id,
+                $energyResourceLog->energyResource->name ?? "<error>Not found</error>",
+                $energyResourceLog->cost,
+                $energyResourceLog->cost == $expenseAmount ? "<info>==</info>" : "<error><></error>",
+                $expense ? "<info>{$expense->amount}</info>" : "<error>Not found</error>"
+            ];
+
+            if (($key + 1) % 10 === 0) {
+                $rows[] = $separator;
+            }
+        }
+
         // Set the contents of the table.
-        $table->setRows([
-            ['https://laravel.com',        'The official Laravel website'],
-            ['https://forge.laravel.com/', 'Painless PHP Servers'],
-            ['https://envoyer.io/',        'Zero Downtime PHP Deployment']
-        ]);
+        $table->setRows($rows);
 
         // Render the table to the output.
         $table->render();
