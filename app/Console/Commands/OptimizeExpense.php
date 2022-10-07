9ad47a61b145162ccd1c65bbb75f170ac4c29789
Marosdee Uma <danya0365@gmail.com>
2022-10-07 23:00:50 +0700

    feat: print log to console

--- app/Console/Commands/OptimizeExpense.php
+++ app/Console/Commands/OptimizeExpense.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Symfony\Component\Console\Helper\Table;
 
 class OptimizeExpense extends Command
 {
@@ -27,6 +28,22 @@ class OptimizeExpense extends Command
      */
     public function handle()
     {
-        return 0;
+        // Create a new Table instance.
+        $table = new Table($this->output);
+
+        // Set the table headers.
+        $table->setHeaders([
+            'Site', 'Description'
+        ]);
+
+        // Set the contents of the table.
+        $table->setRows([
+            ['https://laravel.com',        'The official Laravel website'],
+            ['https://forge.laravel.com/', 'Painless PHP Servers'],
+            ['https://envoyer.io/',        'Zero Downtime PHP Deployment']
+        ]);
+
+        // Render the table to the output.
+        $table->render();
     }
 }
