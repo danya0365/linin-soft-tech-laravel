80bf72266efed3e7928d346f2c73757889d3d24f
Marosdee Uma <danya0365@gmail.com>
2026-01-17 12:19:31 +0700

    fix: api routes

--- routes/api.php
+++ routes/api.php
@@ -23,7 +23,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 // LINE Messaging API Webhook
 Route::post('/line/webhook', [App\Http\Controllers\Api\LineChatbotController::class, 'webhook']);
 
-Route::group(['middleware' => ['supervisor', 'manager', 'admin']], function () {
+Route::group(['middleware' => ['logged-in'], 'prefix' => 'web-chat'], function () {
     Route::post('/message', [App\Http\Controllers\Api\WebChatController::class, 'message']);
     Route::post('/action', [App\Http\Controllers\Api\WebChatController::class, 'action']);
     Route::get('/welcome', [App\Http\Controllers\Api\WebChatController::class, 'welcome']);
