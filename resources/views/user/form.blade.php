4a4633c35ef1239fcd986403ce985a06d899f580
Marosdee Uma <danya0365@gmail.com>
2026-01-21 16:36:54 +0700

    feat: Update user form fields to use select dropdowns for role and customer account, radio buttons for access permissions, and mark name/email as required.

--- resources/views/user/form.blade.php
+++ resources/views/user/form.blade.php
@@ -1,14 +1,72 @@
-<x-crud.form-group name="name" label="Name" type="text" :value="$user->name ?? old('name')" placeholder="Enter Name" />
-<x-crud.form-group name="email" label="Email" type="email" :value="$user->email ?? old('email')" placeholder="Enter Email" />
+<x-crud.form-group name="name" label="Name" type="text" :value="$user->name ?? old('name')" placeholder="Enter Name" required />
+<x-crud.form-group name="email" label="Email" type="email" :value="$user->email ?? old('email')" placeholder="Enter Email" required />
 {{-- Password field: Leave blank to keep existing password when editing --}}
 <x-crud.form-group name="password" label="Password" type="password" :value="''" placeholder="Enter Password (optional for edit)" />
-<x-crud.form-group name="role" label="Role" type="text" :value="$user->role ?? old('role')" placeholder="Enter Role" />
-<x-crud.form-group name="customer_account" label="Customer Account" type="text" :value="$user->customer_account ?? old('customer_account')" placeholder="Enter Customer Account" />
-<x-crud.form-group name="is_can_access_admin" label="Is Can Access Admin" type="checkbox" :value="$user->is_can_access_admin ?? old('is_can_access_admin')" placeholder="Enter Is Can Access Admin" />
-<x-crud.form-group name="is_can_access_manager" label="Is Can Access Manager" type="checkbox" :value="$user->is_can_access_manager ?? old('is_can_access_manager')" placeholder="Enter Is Can Access Manager" />
-<x-crud.form-group name="is_can_access_supervisor" label="Is Can Access Supervisor" type="checkbox" :value="$user->is_can_access_supervisor ?? old('is_can_access_supervisor')" placeholder="Enter Is Can Access Supervisor" />
-<x-crud.form-group name="is_can_access_customer" label="Is Can Access Customer" type="checkbox" :value="$user->is_can_access_customer ?? old('is_can_access_customer')" placeholder="Enter Is Can Access Customer" />
-<x-crud.form-group name="is_can_access_worker" label="Is Can Access Worker" type="checkbox" :value="$user->is_can_access_worker ?? old('is_can_access_worker')" placeholder="Enter Is Can Access Worker" />
+
+{{-- Role Selection --}}
+<x-crud.form-group 
+    name="role" 
+    label="Role" 
+    type="select" 
+    :value="$user->role ?? old('role')" 
+    placeholder="เลือก"
+    :options="$userRoles ?? []" 
+/>
+
+{{-- Customer Account Selection --}}
+@php
+    $customerAccountOptions = [];
+    if (isset($customerAccounts)) {
+        foreach ($customerAccounts as $customerAccount) {
+            $customerAccountOptions[$customerAccount->id] = $customerAccount->name;
+        }
+    }
+@endphp
+<x-crud.form-group 
+    name="customer_account" 
+    label="Customer ID" 
+    type="select" 
+    :value="$user->customer_account ?? old('customer_account')" 
+    placeholder="เลือก"
+    :options="$customerAccountOptions" 
+/>
+
+{{-- Access Permissions - Using Radio Buttons (Yes/No) --}}
+<x-crud.form-group 
+    name="is_can_access_admin" 
+    label="Access Admin" 
+    type="radio" 
+    :value="$user->is_can_access_admin ?? old('is_can_access_admin', '0')" 
+    :options="['1' => 'Yes', '0' => 'No']" 
+/>
+<x-crud.form-group 
+    name="is_can_access_manager" 
+    label="Access Manager" 
+    type="radio" 
+    :value="$user->is_can_access_manager ?? old('is_can_access_manager', '0')" 
+    :options="['1' => 'Yes', '0' => 'No']" 
+/>
+<x-crud.form-group 
+    name="is_can_access_supervisor" 
+    label="Access Supervisor" 
+    type="radio" 
+    :value="$user->is_can_access_supervisor ?? old('is_can_access_supervisor', '0')" 
+    :options="['1' => 'Yes', '0' => 'No']" 
+/>
+<x-crud.form-group 
+    name="is_can_access_customer" 
+    label="Access Customer" 
+    type="radio" 
+    :value="$user->is_can_access_customer ?? old('is_can_access_customer', '0')" 
+    :options="['1' => 'Yes', '0' => 'No']" 
+/>
+<x-crud.form-group 
+    name="is_can_access_worker" 
+    label="Access Worker" 
+    type="radio" 
+    :value="$user->is_can_access_worker ?? old('is_can_access_worker', '0')" 
+    :options="['1' => 'Yes', '0' => 'No']" 
+/>
 
 <div class="flex items-center justify-end gap-3 mt-6 pt-6 border-t border-gray-200 dark:border-gray-700">
     <x-ui.button type="submit" variant="primary" icon="fa fa-save">
