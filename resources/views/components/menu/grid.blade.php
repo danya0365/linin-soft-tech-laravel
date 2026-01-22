0757f684df927d19f4073778ac05ce2f54901952
Marosdee Uma <danya0365@gmail.com>
2026-01-22 11:29:46 +0700

    style: Refine responsive grid column classes and adjust page layout vertical padding.

--- resources/views/components/menu/grid.blade.php
+++ resources/views/components/menu/grid.blade.php
@@ -4,10 +4,10 @@
 
 @php
     $gridClass = match((int)$columns) {
-        2 => 'grid-cols-1 md:grid-cols-2',
-        3 => 'grid-cols-1 sm:grid-cols-2 lg:grid-cols-3',
-        4 => 'grid-cols-1 sm:grid-cols-2 lg:grid-cols-4',
-        default => 'grid-cols-1 sm:grid-cols-2 lg:grid-cols-3'
+        2 => 'grid-cols-2',
+        3 => 'grid-cols-2 lg:grid-cols-3',
+        4 => 'grid-cols-2 md:grid-cols-3 lg:grid-cols-4',
+        default => 'grid-cols-2 lg:grid-cols-3'
     };
 @endphp
 
