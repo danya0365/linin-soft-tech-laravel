996efad7a99a4fb4ff200dc44fb830ca2c706b90
Marosdee Uma <danya0365@gmail.com>
2026-01-27 10:40:45 +0700

    feat: Add `alt` and `class` props to the employee avatar component and implement an `onerror` fallback for image loading.

--- resources/views/components/employee-avatar.blade.php
+++ resources/views/components/employee-avatar.blade.php
@@ -1,1 +1,8 @@
-<img src="{{ $photo ? asset($photo) : asset('assets/avatar_placeholder.png') }}" />
+@props(['photo', 'alt' => 'Employee Avatar', 'class' => 'w-full h-full object-cover'])
+
+<img 
+    src="{{ $photo ? asset($photo) : asset('assets/avatar_placeholder.png') }}" 
+    alt="{{ $alt }}"
+    class="{{ $class }}"
+    onerror="this.onerror=null; this.src='{{ asset('assets/avatar_placeholder.png') }}';"
+/>
